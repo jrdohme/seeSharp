385b52cbb5960380c18e5e61d6ee2e7c90202b5c
jack ohme <jrdohme@gmail.com>
2021-06-22 12:55:42 -0600

    init commit CircleArea

---
+++ CircleArea/CircleArea/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
