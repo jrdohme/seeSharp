3408f7a12b14a31a4e966d0d50a4d93ae249d679
jack ohme <jrdohme@gmail.com>
2021-06-22 12:37:21 -0600

    init commit inputTest

---
+++ InputTest/InputTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InputTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yourName;
+            Console.WriteLine("What is your name?");
+
+            yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}", yourName);
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are {0} years old", age);
+        }
+    }
+}
--- Secondprogram/Secondprogram/Program.cs
+++ Secondprogram/Secondprogram/Program.cs
@@ -6,7 +6,11 @@ namespace Secondprogram
     {
         static void Main(string[] args)
         {
+       
             Console.WriteLine("Hello World!");
+            int x = 5;
+            double y = 20;
+            Console.WriteLine("x = {0}; y = {1}", x, y);
         }
     }
 }
